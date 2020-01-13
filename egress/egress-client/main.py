9740c266253b62826673ba55a7df9a37c2486f4e
mikejeffers <mike.s.jeffers@gmail.com>
2020-01-13 11:46:46 -0500

    docs, comments, local test event for invoke, template cleanup and docs

--- egress/egress-client/main.py
+++ egress/egress-client/main.py
@@ -1,3 +1,10 @@
+'''
+Egress Client Example for the Voxel51 Platform API Webhook Handler.
+
+| Copyright 2017-2020, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+'''
 from voxel51.users.api import API
 from voxel51.users.auth import Token
 import json
@@ -16,45 +23,70 @@ token_dict = {
 
 
 def lambda_handler(event, context):
+    ''' Lambda function entry point, defined in the SAM template.yaml "Handler"
+    This function should parse POST body data for the message from the API's
+    webhook request, which is structured as:
+    {
+        "id":<id of Platform object>
+        "event":<event>
+        "msg":<misc str data about event>
+    }
+    The event key in the request object should only be one of the events this
+    webhook was subscribed to.
+
+    Args:
+        event: object with request data, Platform API request is on event.body
+        context:
+    Returns:
+        Object with "statusCode"
+    '''
     try:
+        # Make sure request has a body
         body = event.get("body")
         if not body:
-            return {
-                "statusCode": 400,
-                "message": "POST missing body!"
-            }
+            print("POST missing body!")
+            return {"statusCode": 400}
+
+        # Check for required keys on json body
         body = json.loads(body)
         job_id = body.get("id")
         event_type = body.get("event")
         msg = body.get("msg")
         if not job_id:
-            return {
-                "statusCode": 400,
-                "message": "POST body missing jobId param"
-            }
+            print("POST body missing jobId param")
+            return {"statusCode": 400}
+
+        # For this example, we are listening to job_complete events.
+        # Only after this event fires can job output be downloaded.
         if "job_complete" != event_type:
-            return {
-                "statusCode": 400,
-                "message": "Job is not in the right state",
-                "event_type": event_type,
-                "msg": msg,
-            }
+            print("Wrong event type: {}".format(event_type))
+            return {"statusCode": 400}
+
+        # AWS Lambda note: the /tmp directory is the only writable part of the
+        # virtual file system during lambda's runtime
+
+        # For this example, we are assuming job outputs are video-labels jsons
         job_output_path = os.path.join("/tmp", job_id + ".json")
+
+        # Create an authenticated connection to the API with your token
         api = API(token=Token(token_dict))
+        # Download the output to local disk
         api.download_job_output(job_id, job_output_path)
         with open(job_output_path, "rb") as read_stream:
+
+            # Upload to S3 bucket for storage
             path_on_bucket = "labels/" + job_id + ".json"
             client = boto3.client("s3")
             client.put_object(Body=read_stream,
                               Bucket="v51-test-egress-268586343424",
                               Key=path_on_bucket)
 
+        # For debug and CloudWatch logs
         print({"msg": msg, "event_type": event_type})
-        return {
-            "statusCode": 200,
-        }
+        # Return 200 on success!
+        return {"statusCode": 200}
+
     except Exception as e:
+        # If anything goes wrong return 500, and view logs for error
         print(e)
-        return {
-            "statusCode": 500,
-        }
+        return {"statusCode": 500}

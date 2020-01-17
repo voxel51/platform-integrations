ab9228cf06f99228916915999d963379b6d6c11e
alan <alan@voxel51.com>
2020-01-17 16:04:56 -0500

    Make sample Slack webhook return a response

--- slack/index.js
+++ slack/index.js
@@ -6,16 +6,20 @@ const webhook = new IncomingWebhook(url);
   that converts a Voxel51 Platform payload and forwards
   it to Slack via a pre-configured webhook.
 */
-module.exports.sendToSlack = (req, res) => {
-  (async () => {
+module.exports.sendToSlack = async (req, res) => {
+  try {
     const formattedData = formatData(req.body);
-    return await webhook.send(formattedData);
-  })();
+    await webhook.send(formattedData);
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send()
+  }
 };
 
 /*
   Formats data coming from the Platform into a format
-  that Slack expects. 
+  that Slack expects.
   "id" and "msg" are fields on the data object coming from the Platform
 */
 const formatData = (data) => {

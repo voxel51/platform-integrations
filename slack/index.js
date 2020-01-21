131ef5fdb37fb81ae6b528c756d8aab3c85598c9
brimoor <brimoor@umich.edu>
2020-01-21 11:38:27 -0500

    editing pass over README

--- slack/index.js
+++ slack/index.js
@@ -1,27 +1,20 @@
+/**
+ * Example middleware that converts Voxel51 Platform webhook payloads to
+ * Slack webhook notifications.
+ *
+ * Copyright 2017-2020, Voxel51, Inc.<br>
+ * {@link https://voxel51.com voxel51.com}
+ */
 const { IncomingWebhook } = require('@slack/webhook');
+
 const url = process.env.SLACK_WEBHOOK_URL;
 const webhook = new IncomingWebhook(url);
-/*
-  Example client application (integration middleware)
-  that converts a Voxel51 Platform payload and forwards
-  it to Slack via a pre-configured webhook.
-*/
-module.exports.sendToSlack = async (req, res) => {
-  try {
-    const formattedData = formatData(req.body);
-    await webhook.send(formattedData);
-    res.status(204).send();
-  } catch (error) {
-    console.error(error);
-    res.status(500).send()
-  }
-};
 
 /*
-  Formats data coming from the Platform into a format
-  that Slack expects.
-  "id" and "msg" are fields on the data object coming from the Platform
-*/
+ * Converts data from a Voxel51 Platform payload into the format that Slack
+ * expects. `id` and `msg` are fields on the data object coming from the
+ * Platform.
+ */
 const formatData = (data) => {
   return {
     blocks: [
@@ -48,4 +41,19 @@ const formatData = (data) => {
       },
     ],
   };
+};
+
+/*
+ * Example client application (integration middleware) that receives a Voxel51
+ * Platform payload and forwards it to Slack via a pre-configured webhook.
+ */
+module.exports.sendToSlack = async (req, res) => {
+  try {
+    const formattedData = formatData(req.body);
+    await webhook.send(formattedData);
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send()
+  }
 };
